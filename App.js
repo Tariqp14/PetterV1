ad05515a69c6b9fd76b62325df87900bbfbec72e
Jared <jaredjef4son@gmail.com>
2025-01-24 09:32:06 -0500

    added basic navigation to view the screens and added dependencies

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Navigation from './Navigation';
+
 //currently trying to keep main app.js clear to avoid too much confusion -T
 // video for navbar creation https://www.youtube.com/watch?v=AnjyzruZ36E
 //https://reactnavigation.org/docs/bottom-tab-navigator
+
 export default function App() {
   return (
-    <View style={styles.container}>
+    <Navigation/>
+    // Just commenting this out so I can test the navigation
+    /* <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-    </View>
+    </View> */
   );
 }
 
---
+++ Navigation.js
@@ -0,0 +1,27 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Screen } from '@react-navigation/elements';
+import Calendar from "./pages/Calendar";
+import Dates from "./pages/Calendar";
+import TimelineCalendarScreen from "./pages/timelineCalendarScreen";
+import Form from "./pages/formPage";
+
+//bottom tab
+/* const Tab = createBottomTabNavigator
+
+function TabGroup() {
+    return(
+        <Tab.Navigator>
+            <Tab.Screen name="Calendar" component={Calendar} />
+        </Tab.Navigator>
+    );
+} */
+
+//Function to create bottom nav bar
+export default function Navigation() {
+    return (
+        <NavigationContainer>
+            <TimelineCalendarScreen/>
+        </NavigationContainer>
+    )
+}
